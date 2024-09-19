750f1a3166e7735d1d420ceceb940710ec41a3da
Different <--help>
2024-09-19 15:52:50 +1000

    Update pictures directory and vrchat directory for symlink

--- home-manager/programs/steam.nix
+++ home-manager/programs/steam.nix
@@ -1,6 +1,9 @@
-{config, ...}:{
-  # link vrchat pictures to pictures folder
-systemd.user.tmpfiles.rules = [
-  "L ${config.home.homeDirectory}/Pictures/VRChat - - - - ${config.home.homeDirectory}/.steam/root/steamapps/compatdata/438100/pfx/drive_c/users/steamuser/Pictures/VRChat"
-];
+{config, ...}: {
+  systemd.user.tmpfiles.rules = let
+    inherit (config.home) homeDirectory;
+    vrchatPictures = "${homeDirectory}/media/.steam/steamapps/compatdata/438100/pfx/drive_c/users/steamuser/Pictures/VRChat";
+  in [
+    # link vrchat pictures to pictures folder
+    "L ${homeDirectory}/pictures/vrchat - - - - ${vrchatPictures}"
+  ];
 }
