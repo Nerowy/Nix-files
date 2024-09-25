53d0a712d64c55ab389bd0258740ff4a0c708434
Nerowy <Nerowy33@gmail.com>
2024-09-25 13:49:10 +0100

    Fixes: #24

--- nixos/programs/steam.nix
+++ nixos/programs/steam.nix
@@ -5,9 +5,9 @@
       proton-ge-bin
       (proton-ge-bin.overrideAttrs (finalAttrs: {
         pname = "proton-ge-rtsp-bin";
-        version = "GE-Proton9-10-rtsp13";
+        version = "GE-Proton9-11-rtsp15";
         src = pkgs.fetchzip {
-          url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/GE-Proton9-10-rtsp13/GE-Proton9-10-rtsp13.tar.gz";
+          url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/tag/GE-Proton9-11-rtsp15";
           hash = "sha256-jf1p33Kuqtriycf6gOw/IBdx/ts/P7PJd+pjxonAS/U=";
         };
       }))
